3fd7d29b11b88f08ba191737c697ff0f3de24c7f
Islam <hsantanasilva@hotmail.com>
2022-03-07 22:30:34 -0300

    Add Project Files

---
+++ Prouj/main.js
@@ -0,0 +1,18 @@
+//* Função de clique do menu *//
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+//* Função de clique das listas do menu *//
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
